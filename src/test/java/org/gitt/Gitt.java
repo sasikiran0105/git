380c6c350f049b5fa6a90bde9bb16d7c93aa7ccf
sasikiran0105 <sasikiransusanth@gmail.com>
2022-05-27 20:30:12 +0530

    methods

---
+++ src/test/java/org/gitt/Gitt.java
@@ -0,0 +1,28 @@
+package org.gitt;
+
+public class Gitt {
+	
+	
+		public void method1() {
+			System.out.println("sasi");
+		}
+		public void method2() {
+			System.out.println("kiran");
+		}
+		public void method3() {
+			System.out.println("susanth");
+		}
+		public void method4() {
+			System.out.println("vinoth");
+		}
+		public void method5() {
+			System.out.println("yogesh");
+		}
+		
+		
+		
+
+	}
+
+
+
